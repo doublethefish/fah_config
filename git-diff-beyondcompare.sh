770fba27470da057ff377256a2df1470abb4f644
Frank Harrison <frank@doublethefish.com>
2014-12-26 13:47:47 +0000

    adding the beyond compare util - may already exist

---
+++ git-diff-beyondcompare.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+##!/bin/sh
+## diff is called by git with 7 parameters:
+## path old-file old-hex old-mode new-file new-hex new-mode
+bcompare "$2" "$5" -title1="$2 (Prev)" -title2="$5 (Working)" | cat
+#echo "1 = $1"
+#echo "2 = $2"
+#echo "3 = $3"
+#echo "4 = $4"
+#echo "5 = $5"
+#echo "6 = $6"
+#echo -e "7 = $7\n\n"
+exit 0
+
