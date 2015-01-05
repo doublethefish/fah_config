045fcb0a4bed953f69d0a9f69f85aa79403cf54b
Frank Harrison <frank@doublethefish.com>
2015-01-05 20:44:48 +0000

    getting the bash-completion script to search in XCode as well as linux install locations

--- bash/git_utils.sh
+++ bash/git_utils.sh
@@ -2,5 +2,14 @@
 alias gd="git difftool -y"
 alias dg="gd"
 
-source /usr/local/git/contrib/completion/git-completion.bash
+GIT_SEARCH_DIRS="/Applications/Xcode.app/Contents/Developer/usr/share/git-core /usr/local/git/contrib/completion"
+for GIT_DIR in ${GIT_SEARCH_DIRS}; do
+  GIT_SH_FILES="git-completion.bash git-prompt.sh"
+  for GIT_SH in ${GIT_SH_FILES}; do
+    GIT_SCRIPT=${GIT_DIR}/${GIT_SH}
+    if [ -f  ${GIT_SCRIPT} ]; then
+      source ${GIT_SCRIPT}
+    fi
+  done;
+done;
 
