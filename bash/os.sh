f340ce9a74087c01bbca87937a0f4fea1025da88
Frank Harrison <frank@doublethefish.com>
2015-01-06 18:59:09 +0000

    now that I'm on mac I'm adding the mac-platform support to the bash config

--- bash/os.sh
+++ bash/os.sh
@@ -5,5 +5,7 @@ if [[ "$unamestr" == 'Linux' ]]; then
   FAH_PLATFORM='linux'
 elif [[ "$unamestr" == 'FreeBSD' ]]; then
   FAH_PLATFORM='freebsd'
+elif [[ "$unamestr" == 'Darwin' ]]; then
+  FAH_PLATFORM='mac'
 fi
 echo $unamestr
