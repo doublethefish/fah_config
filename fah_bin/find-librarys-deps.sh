71f2d270089594fc2cf0806c1d4fee995a0d66c4
Frank Harrison <frank@doublethefish.com>
2015-09-27 11:30:44 +0100

    extending the library dependcy searcher so it works on Linux (readelf)

--- fah_bin/find-librarys-deps.sh
+++ fah_bin/find-librarys-deps.sh
@@ -1,3 +1,17 @@
+echo "$FAH_PLATFORM"
+
+if [[ -z $FAH_PLATFORM ]]; then
+  echo "FAH_PLATFORM not set!"
+  exit 2
+fi
+
+if [[ "$FAH_PLATFORM" == 'mac' ]]; then
+  LIB_DEPS_CMD="/usr/bin/otool -L "
+elif [[ "$FAH_PLATFORM" == 'linux' ]]; then
+  LIB_DEPS_CMD="readelf -d | grep NEEDED | sed 's/.*\[\(.*\)\]/\1/g'"
+else
+  echo "Unsupported platform! ${FAH_PLATFORM}"
+fi
 
 searchLib=$1
 
@@ -8,7 +22,7 @@ IFS='
 '
 for lib in `find . -name "*.dylib" -o -name "*.so"`
 do
-  for path in `/usr/bin/otool -L ${FCWD}/${lib} | tail -n +2 | grep "${searchLib}"`
+  for path in `echo ${FCWD}/${lib} | xargs ${LIB_DEPS_CMD} | tail -n +2 | grep "${searchLib}"`
   do
     echo -e "\t${lib} : ${path}"
   done
