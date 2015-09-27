12fbb25e2fc06956ad5fe14a44bf2d5369d283dd
Frank Harrison <frank@doublethefish.com>
2015-09-27 10:44:15 +0100

    configuring the svn-diff commands to be specific per platform

--- bash/svn_utils.sh
+++ bash/svn_utils.sh
@@ -6,6 +6,25 @@ fi
 
 # svn diff
 alias kdiff3="/Developer/Applications/Utilities/kdiff3.app/Contents/MacOS/kdiff3"
-alias sd='svn diff --diff-cmd=${FAH_BIN}/svn-diff-beyondcompare.sh'
-#alias sd="svn diff --diff-cmd ~/bin/svn_diff_mac.bash"
+
+if [[ "${FAH_PLATFORM}" == "mac" ]]; then
+  # diffmerge is pretty good but opendiff (aka Filemerge) is better
+  #eval DIFF_CMD=${FAH_BIN}/svn-diff-mac-diffmerge.sh
+  eval DIFF_CMD=${FAH_BIN}/svn-diff-mac-opendiff.sh
+elif [[ "${FAH_PLATFORM}" == "linux" ]]; then
+  # TODO: check that beyond compare works
+  eval DIFF_CMD=${FAH_BIN}/svn-diff-beyondcompare.sh
+else
+  # default to vimdiff for the mo
+  # do - diff obtain
+  # dp - diff put
+  eval DIFF_CMD=${FAH_BIN}/svn-diff-vimdiff.sh
+fi
+
+# setup the sd command
+alias sd="svn diff --diff-cmd=${DIFF_CMD}"
+
+if [[ -e /opt/subversion/bin/ ]]; then
+  PATH=${PATH}:/opt/subversion/bin/
+fi
 
