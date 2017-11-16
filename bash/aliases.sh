84abe7a5a03e01de54d2a9ab51c98ca225c6faca
Frank Harrison <frank@doublethefish.com>
2017-11-16 11:21:52 +0000

    Remove the mac-specific ls option on non-mac systems

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,6 +1,13 @@
+OS_SPECIFIC_LS_OPTION=""
+# Mac OS specific aliases
+if [[ "$FAH_PLATFORM" == 'mac' ]]; then
+  alias gvim="mvim"
+  OS_SPECIFIC_LS_OPTION="O"
+fi
+
 # A place to store useful aliases
-alias ll="ls -GlFhkO"
-alias la="ls -GAlFhkO"
+alias ll="ls -GlFhk${OS_SPECIFIC_LS_OPTION}"
+alias la="ls -GAlFhk${OS_SPECIFIC_LS_OPTION}"
 alias ffind="find . -name "
 alias ffindi="find . -iname "
 alias hgrep="ffind \"*.h\" | xargs grep -n"
@@ -9,11 +16,6 @@ alias fah="dev && cd fah_products"
 alias dtf="dev && cd doublethefish"
 alias double="dtf"
 alias doublethefish="dtf"
-
-# Mac OS specific aliases
-if [[ "$FAH_PLATFORM" == 'mac' ]]; then
-  alias gvim="mvim"
-fi
 
 
 function vffind {
