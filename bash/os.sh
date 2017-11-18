7875b0bc8e8a4699617ce15f4a5752729ab50444
Frank Harrison <frank@doublethefish.com>
2017-11-18 09:55:06 +0000

    Use a per-system config file with warnings, based on a template file Also fix up ssh-key adding

--- bash/os.sh
+++ bash/os.sh
@@ -13,6 +13,19 @@ fi
 export FAH_PLATFORM
 echo "Setting FAH_PLATFORM to ${FAH_PLATFORM} from ${unamestr}"
 
-if [ -z ${FAH_DEV_DIR} ]; then
-  echo "WARNING! : You have not set the 'FAH_DEV_DIR' env variable on this machine, yet."
+FAH_SYSTEM_CONFIG=${FAH_HOME}/.bash_fah_system
+if [ ! -f "${FAH_SYSTEM_CONFIG}" ]; then
+  cp ${FAH_HOME}/.fah/bash/bash_fah_system.template ${FAH_SYSTEM_CONFIG}
+  echo "WARNING! : Copied the system-specific to ${FAH_SYSTEM_CONFIG}, you will"
+  echo "         : need to update it"
+fi
+
+source ${FAH_SYSTEM_CONFIG} # pull in the system configuration
+
+if [[ -z ${FAH_DEV_DIR} ]] || [[ ! -d  ${FAH_DEV_DIR} ]]; then
+  echo "WARNING! : You have not set or created the 'FAH_DEV_DIR' env variable"
+  echo "         : on this machine, yet. Check ${FAH_SYSTEM_CONFIG}"
+  if [[ ! -d ${FAH_DEV_DIR} ]]; then
+    echo "         : Not found ${FAH_DEV_DIR}"
+  fi
 fi
--- bash/ssh.sh
+++ bash/ssh.sh
@@ -1,16 +1,26 @@
 # hmm does work at all times...
 SSH_ENV="$HOME/.ssh/environment"
 
+if [ -z "${FAH_SSH_KEYS}" ]; then
+  echo "WARNING: FAH_SSH_KEYS env variable not set!"
+fi
+
 function start_agent {
     echo "Initialising new SSH agent..."
     /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
     echo succeeded
     chmod 600 "${SSH_ENV}"
     . "${SSH_ENV}" > /dev/null
-    /usr/bin/ssh-add;
+    for KEY_PATH in ${FAH_SSH_KEYS}; do
+      eval KEY_PATH=${KEY_PATH}
+      if [[ ! -f  ${KEY_PATH} ]]; then
+        echo "WARNING!: SSH Key: not found ${KEY_PATH}, you may need to update ${FAH_SYSTEM_CONFIG}"
+      else
+        echo "SSH Key: adding ${KEY_PATH}"
+        /usr/bin/ssh-add ${KEY_PATH}
+      fi
+    done;
 }
-
-# Source SSH settings, if applicable
 
 if [ -f "${SSH_ENV}" ]; then
     . "${SSH_ENV}" > /dev/null
@@ -21,4 +31,3 @@ if [ -f "${SSH_ENV}" ]; then
 else
     start_agent;
 fi
-
