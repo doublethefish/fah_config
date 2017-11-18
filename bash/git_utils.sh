7208ad68ba7ae1442d38dbfe3650bb0ed1c48e49
Frank Harrison <frank@doublethefish.com>
2017-11-18 09:20:31 +0000

    Fix git-prompt & git-complete search on mac
    * Remove hard-coded brew paths
    * Use the brew-search dir properly
    * Remove the sourcing of the git-complete search dir, which is now defunct
    * Ensure the correct path variables are set and testied properly

--- bash/git_utils.sh
+++ bash/git_utils.sh
@@ -7,16 +7,16 @@ fi
 alias gd="git difftool -y"
 alias dg="gd"
 
-GIT_SEARCH_DIRS="/Applications/Xcode.app/Contents/Developer/usr/share/git-core /usr/local/git/contrib/completion"
+GIT_SEARCH_DIRS="/usr/local/git/contrib/completion"
 
 if [[ "${FAH_PLATFORM}" == "mac" ]]; then
-  GIT_SEARCH_DIRS="$GIT_SEARCH_DIRS $(brew --prefix bash-git-prompt)"
+  BREW_GIT_DIR=$(brew --prefix bash-git-prompt)/share
+  GIT_SEARCH_DIRS="$GIT_SEARCH_DIRS ${BREW_GIT_DIR}"
 fi
 
 GIT_COMPLETION_FILES="git-completion.bash"
 GIT_PROMPT_FILES="gitprompt.sh git-prompt.sh"
 unset GIT_PROMPT
-unset GIT_COMPLETE
 for GIT_DIR in ${GIT_SEARCH_DIRS}; do
   if [ ! -e ${GIT_DIR} ]; then
     continue
@@ -24,33 +24,16 @@ for GIT_DIR in ${GIT_SEARCH_DIRS}; do
 
   if [ -z ${GIT_PROMPT} ]; then
     for GIT_PROMPT_CAND in ${GIT_PROMPT_FILES}; do
-      GIT_PROMPT=${GIT_DIR}/${GIT_PROMPT_CAND}
-    done;
-  fi
-
-  if [ -z ${GIT_PROMPT} ]; then
-    for GIT_COMPLETE_CAND in ${GIT_COMPLETION_FILES}; do
-      GIT_COMPLETE=${GIT_DIR}/${GIT_COMPLETE_CAND}
-    done;
+      CAND=${GIT_DIR}/${GIT_PROMPT_CAND}
+      if [ -f ${CAND} ]; then
+        GIT_PROMPT=${CAND}
+      fi
+    done
   fi
 done;
 
-GIT_PROMPT_THEME=Default
-
-if [ ! -z  ${GIT_PROMPT} ]; then
+if [[ ! -z ${GIT_PROMPT} ]] || [[ -f  ${GIT_PROMPT} ]]; then
   source ${GIT_PROMPT}
-elif [[ "${FAH_PLATFORM}" == 'mac' ]]; then
-	# use Brew to look for git stuffs on mac
-	if [ -f "$(brew --prefix bash-git-prompt)/share/gitprompt.sh" ]; then
-		GIT_PROMPT_THEME=Default
-		source "$(brew --prefix bash-git-prompt)/share/gitprompt.sh"
-	else
-		echo "Git-prompt script not found: ${GIT_PROMPT_FILES}"
-	fi
-fi
-
-if [ ! -z  ${GIT_COMPLETE} ]; then
-  source ${GIT_COMPLETE}
 else
-	echo "Git-complete script not found: ${GIT_COMPLETION_FILES}"
+  echo "Git-prompt script not found: ${GIT_COMPLETION_FILES}"
 fi
