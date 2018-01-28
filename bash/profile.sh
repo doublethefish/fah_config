216d6dc8b2a7e9b96fdccc1c6e3baf80fef17b84
Frank Harrison <frank@doublethefish.com>
2018-01-28 17:48:37 +0000

    Removes trailing slash from PATH entry

--- bash/profile.sh
+++ bash/profile.sh
@@ -8,7 +8,7 @@ source ~/.fah/bash/bash.sh # shared bashrc-like config
 #----------------------------
 # MacPorts Installer addition on 2009-12-18_at_18:26:41: adding an appropriate PATH variable for use with MacPorts.
 PATH=/opt/local/bin:/opt/local/sbin:$PATH
-PATH=${PATH}:/usr/local/mysql-5.5.2-m2-osx10.6-x86_64/bin/
+PATH=${PATH}:/usr/local/mysql-5.5.2-m2-osx10.6-x86_64/bin
 # Finished adapting your PATH environment variable for use with MacPorts.
 
 ####################################
