ccd894e8432be3f12d021c701cc013dbb266fc51
Frank Harrison <frank@doublethefish.com>
2018-07-01 14:02:42 +0100

    Fixes bash linting issues by running shfmt on .bash files

--- bin/git/git-rebase-all-branches.bash
+++ bin/git/git-rebase-all-branches.bash
@@ -1,31 +1,30 @@
-for branch in `git for-each-ref --shell --format="%(refname)" refs/heads/ | sed 's|refs/heads/||g' | sed s/\'//g`; 
-do 
-  echo ""
-  echo "==============================================================================="
-  echo "Rebasing : '$branch'"
-  echo "==============================================================================="
-  git checkout $branch && git rebase master # || break; 
+for branch in $(git for-each-ref --shell --format="%(refname)" refs/heads/ | sed 's|refs/heads/||g' | sed s/\'//g); do
+	echo ""
+	echo "==============================================================================="
+	echo "Rebasing : '$branch'"
+	echo "==============================================================================="
+	git checkout $branch && git rebase master # || break;
 
-  ret=$?
-  while [[ $ret -gt 0 ]]
-  do
-    echo ""
-    echo "==============================================================================="
-    echo "Rebasing : '$branch' completed with $ret"
-    echo "==============================================================================="
+	ret=$?
+	while
+		[[ $ret -gt 0 ]]
+	do
+		echo ""
+		echo "==============================================================================="
+		echo "Rebasing : '$branch' completed with $ret"
+		echo "==============================================================================="
 
-    echo $1
-    if [ "$1" = "skip" ]; then
-      echo "aborting"
-      git rebase --abort
-      ret=0
-    else
-      echo "merging"
-      git mergetool && git rebase --continue || git rebase --skip
-      ret=$?
-    fi
+		echo $1
+		if [ "$1" = "skip" ]; then
+			echo "aborting"
+			git rebase --abort
+			ret=0
+		else
+			echo "merging"
+			git mergetool && git rebase --continue || git rebase --skip
+			ret=$?
+		fi
 
-  done
+	done
 
-done;
-
+done
