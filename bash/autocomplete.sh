eec7bc9afe1f2dfa0ef64b457b4d1329ef9a0a67
Frank Harrison <frank@doublethefish.com>
2018-11-04 11:36:34 +0000

    Removes brew-specific code on mac that makes loading TTY instances slow

--- bash/autocomplete.sh
+++ bash/autocomplete.sh
@@ -1,7 +1,7 @@
 # enable programmable completion features (you don't need to enable
 # this, if it's already enabled in /etc/bash.bashrc and /etc/profile
 # sources /etc/bash.bashrc).
-for filename in /etc/bash_completion /etc/bash_completion.d/git /etc/bash_completion.d/subversion /etc/bash_completion.d/yum.bash; do
+for filename in /etc/bash_completion /etc/bash_completion.d/git /etc/bash_completion.d/subversion /etc/bash_completion.d/yum.bash /usr/local/etc/bash_completion; do
 	if [ -f $filename ] && ! shopt -oq posix; then
 		. $filename
 	fi
