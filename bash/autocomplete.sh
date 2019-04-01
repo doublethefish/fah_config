8d2c36ad5628d87e676b36e6495e5ad742e657cd
Frank Harrison <frank@doublethefish.com>
2019-04-01 22:35:16 +0100

    Uses the correct completion script on mac

--- bash/autocomplete.sh
+++ bash/autocomplete.sh
@@ -1,7 +1,7 @@
 # enable programmable completion features (you don't need to enable
 # this, if it's already enabled in /etc/bash.bashrc and /etc/profile
 # sources /etc/bash.bashrc).
-for filename in /etc/bash_completion /etc/bash_completion.d/git /etc/bash_completion.d/subversion /etc/bash_completion.d/yum.bash /usr/local/etc/bash_completion; do
+for filename in /etc/bash_completion /etc/bash_completion.d/git /etc/bash_completion.d/subversion /etc/bash_completion.d/yum.bash /usr/local/etc/bash_completion /usr/local/etc/profile.d/bash_completion.sh; do
 	if [ -f $filename ] && ! shopt -oq posix; then
 		. $filename
 	fi
