bd504c1b17441ed54238e6059905a30536e8b683
Frank Harrison <frank@doublethefish.com>
2019-04-01 22:43:07 +0100

    Adds git log/graph aliases

--- bash/git_utils.sh
+++ bash/git_utils.sh
@@ -8,3 +8,5 @@ fi
 # aliases for git that don't fit intp any of the git configs
 alias gd="git difftool -y"
 alias dg="gd"
+alias gg="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+alias gg2="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all"
